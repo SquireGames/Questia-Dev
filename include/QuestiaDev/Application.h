39622bc5a5a43496faff7531e0378320b5c884fd
Matthew Kaminski <kaminskimateusz7@gmail.com>
2017-03-12 20:25:03 -0400

    Started using Questia engine

--- include/QuestiaDev/Application.h
+++ include/QuestiaDev/Application.h
@@ -1,40 +1,21 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
 
-#include <SFML/Graphics.hpp>
 #include <iostream>
 
-#include "QuestiaEng/Data/InputBuffer.h"
-
-#include "QuestiaEng/Data/Data_Desktop.h"
-
-#include "QuestiaEng/Utl/SaveFile/SaveFile_Options.h"
-
-#include "QuestiaEng/StateManager/StateManager.h"
-#include "QuestiaEng/StateManager/States/State_Transition.h"
-
+#include "QuestiaEng/Engine.h"
 
 class Application
 {
 public:
+	//ctor + dtor
     Application();
     ~Application();
-    void run();
-
-private:
-    void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
-    void processEvents();
-    void update(sf::Time elapsedTime);
-    void updateStatistics(sf::Time elapsedTime);
-    void render();
 	
-	InputBuffer inputBuffer;
-
-    sf::RenderWindow window_main;
-    SaveFile_Options saveFile;
-    const sf::Time timePerFrame = sf::seconds(1.f / 128.f);
-
-    sf::Time mStatisticsUpdateTime;
-    int mStatisticsFramesCount;
+	//start of the application
+    void run();
+	
+private:
+	Engine eng;
 };
 #endif // APPLICATION_H
